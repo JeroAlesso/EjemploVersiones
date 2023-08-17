e649e12ffad79c85cec340b692225baefea77e52
JeroAlesso <jero.alesso@icloud.com>
2023-08-17 18:41:41 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/ejemploversiones/EjemploVersiones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemploversiones;
+
+/**
+ *
+ * @author Athlon 3000G
+ */
+public class EjemploVersiones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
